271911bb23568049cd2c6214f18a87b4b49d6912
Raden Mas Abdul <mac@MacBook-Air.local>
2025-10-24 17:20:14 +0700

    feat: add Prisma schema for User model

---
+++ prisma/migrations/20251024100452_users/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(255) NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "role" VARCHAR(50) NOT NULL,
+    "avatar_url" VARCHAR(255),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
