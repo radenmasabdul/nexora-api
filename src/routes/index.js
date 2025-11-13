5b77d05773931679f23c03dd4f1ef8684285f0b3
Raden Mas Abdul <mac@MacBook-Air.local>
2025-11-13 21:53:51 +0700

    refactor: remove Team module completely to fix prisma client errors

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,8 @@ const router = express.Router();
 
 const authRoutes = require("./auth/authRoutes");
 const userRoutes = require("./user/userRoutes");
-const teamRoutes = require("./team/teamRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
-router.use("/teams", teamRoutes);
 
 module.exports = router;
