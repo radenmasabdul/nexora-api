01a6b9f816446f0443fe3ffbffa78ca69e42d1b8
Raden Mas Abdul <mac@MacBook-Air.local>
2025-11-17 16:39:02 +0700

    feat(task): implement Task Module with CRUD operation

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,11 +6,13 @@ const userRoutes = require("./user/userRoutes");
 const teamRoutes = require("./team/teamRoutes");
 const memberRoutes = require("./member/memberRoutes");
 const projectsRoutes = require("./projects/projectsRoutes");
+const taskRoutes = require("./task/taskRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/teams", teamRoutes);
 router.use("/members", memberRoutes);
 router.use("/projects", projectsRoutes);
+router.use("/tasks", taskRoutes);
 
 module.exports = router;
